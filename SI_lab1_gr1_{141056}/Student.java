91ba5095f5ab2ff6312d740df2d98cf02816e945
VasilStojanovski95 <63552147+VasilStojanovski95@users.noreply.github.com>
2020-04-12 20:56:32 +0200

    student class finished

--- SI_lab1_gr1_{141056}/Student.java
+++ SI_lab1_gr1_{141056}/Student.java
@@ -1,18 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class Student {
 	String index;
 	String firstName;
 	String lastName;
-	List<int> labPoints = new List<int>();
+	List<int> labPoints = new ArrayList<>();
 	
-	//TODO constructor
-	
+	//constructor
+	public Student(int index, String firstName, String lastName)
+	{
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
 	//TODO seters & getters
-
+	public String setIndex(String index) { this.index = index;}
+	public String setFirstName(String firstName) {this.firstName = firstName;}
+	public String setLastName(String firstName) {this.lastName = lastName;}
+	
+	public String getIndex() { return this.index;}
+	public String getFirstName() { return this.firstName;}
+	public String getLastName() { return this.lastName;}
+	
 	public double getAverage() {
 		//TODO
+		int total = 0;
+		int size = labPoints.size();
+		labPoints.forEach(p -> total += p);
+		double avg = total/size;
+		return avg;
 	}
 
 	public boolean hasSignature() {
-		//TODO
+		if (labPoints.size() >= 8) 
+			{
+				return true;
+			}
+		else 
+			{
+				return false;
+			}
 	}
 }
